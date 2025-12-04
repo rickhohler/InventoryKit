9c11e636a2aa3b5341a8ba10dd0bc017fa94c5c9
Rick Hohler <rick.hohler@gmail.com>
2025-12-04 10:16:17 -0600

    chore: Update DesignAlgorithmsKit to v1.0.3 (#9)
    * chore: Update DesignAlgorithmsKit to v1.0.2
    
    - Updated DesignAlgorithmsKit dependency from 1.0.0 to 1.0.2
    
    * chore: Update DesignAlgorithmsKit to latest release v1.0.3
    
    - Updated dependency from 1.0.2 to 1.0.3

--- Package.swift
+++ Package.swift
@@ -18,13 +18,15 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/jpsim/Yams.git", from: "5.1.2")
+        .package(url: "https://github.com/jpsim/Yams.git", from: "5.1.2"),
+        .package(url: "https://github.com/rickhohler/DesignAlgorithmsKit.git", from: "1.0.3"),
     ],
     targets: [
         .target(
             name: "InventoryKit",
             dependencies: [
-                .product(name: "Yams", package: "Yams")
+                .product(name: "Yams", package: "Yams"),
+                .product(name: "DesignAlgorithmsKit", package: "DesignAlgorithmsKit"),
             ]
         ),
         .testTarget(
